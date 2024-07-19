5a5548a9eb48425e03141781e351feabf7cf308b
dorea <dodomokhtari@hotmail.fr>
2024-07-19 13:59:57 +0200

    login init

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,4 +1,23 @@
-const email = document.getElementById("form #name")
-const password = document.getElementById("form #email")
-const form = document.getElementById("form")
-const error = document.querySelector(".login p")
+
+const email = document.querySelector("form #email");
+const password = document.querySelector("form #password");
+console.log(email,password);
+const form = document.querySelector("form");
+const erreur = document.querySelector(".login p");
+console.log(erreur);
+
+async function getUsers() {
+  const answer = await fetch("http://localhost:5678/api/users/login");
+  return await answer.json();
+}
+getUsers();
+
+
+
+//connexion//
+
+async function login() {
+  const users = await getUsers;
+  console.log(users);
+}
+login();
--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -3,33 +3,39 @@ const filters = document.querySelector(".filters");
 
 async function getWorks() {
   const answer = await fetch("http://localhost:5678/api/works");
-  return await  answer.json();
- }
+  return await answer.json();
+}
 getWorks();
 
 
 
-async function displayWorks () {
-const arrayWorks = await getWorks();
-arrayWorks.forEach((project) => {
-  const figure= document.createElement("figure");
+async function displayWorks() {
+  const projects = await getWorks();
+  projects.forEach((project) => {
+    createprojects(project);
+  });
+}
+displayWorks();
+
+function createprojects(project) {
+  const figure = document.createElement("figure");
   const img = document.createElement("img");
   const figcaption = document.createElement("figcaption");
-  img.src= project.imageUrl;
+  img.src = project.imageUrl;
   figcaption.innerHTML = project.title;
   figure.appendChild(img);
   figure.appendChild(figcaption);
   gallery.appendChild(figure);
-console.log("ca marche");
-});
-
+  console.log("ca marche");
 }
-displayWorks();
+
+
+
 
 
 /*******affichage filtres***************** */
 
-async function getCatergories(){
+async function getCatergories() {
   const response = await fetch("http://localhost:5678/api/categories");
   return await response.json();
 }
@@ -40,34 +46,45 @@ async function Buttons() {
   const categories = await getCatergories();
   console.log(categories);
 
-categories.forEach(category => {
-const btn = document.createElement("button");
-btn.innerHTML= category.name.toUpperCase();
-btn.id=category.id;
-filters.appendChild(btn);
-})}
+  categories.forEach(category => {
+    const btn = document.createElement("button");
+    btn.innerHTML = category.name.toUpperCase();
+    btn.id = category.id;
+    filters.appendChild(btn);
+  })
+}
 
 Buttons();
 
 
 ////filtrer///
 
-async function filterCategories () {
+async function filterCategories() {
   const AllWorks = await getWorks();
   const buttons = document.querySelectorAll(".filters button");
   buttons.forEach(button => {
-    button.addEventListener("click",(e) => {
+    button.addEventListener("click", (e) => {
       btnId = e.target.id;
-      gallery.innerHTML = "";
-      console.log(btnId);
+      gallery.innerHTML = ""; /// suppression de la gallerie avant filtrage//
+      if (btnId !== "0") {
+        const worksfilteredcategory = AllWorks.filter(project => {
+          return project.categoryId == btnId;
+        });
+        //tableau trié mais pas affiché//
+        worksfilteredcategory.forEach(project => {
+          createprojects(project);
 
-    });
-  } );
+        });
+      }
+      else {
+        displayWorks();  //tous//
+      }
+      
+    console.log(btnId);
 
-
-
-  
-
+  });
+});
 }
+
 
 filterCategories();
